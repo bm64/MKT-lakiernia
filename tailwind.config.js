99d19f5442fcc47581e60706c3869d8372ac3270
Kamil Jakubus <kamiljkbz@protonmail.com>
2019-11-23 16:39:52 +0100

    Override basic theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        black: "#1B1B1E",
+        white: "#fbfbfb",
+        orange: "#FFAE03",
+        gray: "#515052"
+      },
       minHeight: {
-        'screen-3/4': '75vh',
+        "screen-3/4": "75vh"
       },
       maxWidth: {
-        'screen-xl': '1280px',
-      },
-    },
+        "screen-xl": "1280px"
+      }
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 };
