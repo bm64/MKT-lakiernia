fe317f2b9880419becad124503984f4f7b5f563b
Kamil Jakubus <kamiljkbz@protonmail.com>
2019-11-23 16:34:23 +0100

    Remove redundant text sizes for h[1-4]

--- components/header.js
+++ components/header.js
@@ -6,9 +6,9 @@ function Header({}) {
   const showMenu = useState(false);
 
   return (
-    <div className="absolute top-0 z-10 w-full bg-transparent">
-      <div className="z-10 max-w-screen-xl flex justify-between bg-transparent items-center h-16 mx-auto px-3">
-        <h1 className="text-2xl font-bold text-gray-300">MKT SERWIS</h1>
+    <div className="absolute top-0 w-full bg-transparent z-10">
+      <div className="max-w-screen-xl flex justify-between items-center bg-transparent z-10 h-16 mx-auto px-3">
+        <h1 className="font-bold text-gray-300">MKT SERWIS</h1>
         <FaBars className="text-4xl text-gray-300 cursor-pointer" />
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,22 @@ function Home() {
           className="bg-cover"
           style={{ backgroundImage: "url(/mobile1.jpeg)" }}
         >
-          <div className="min-h-screen-3/4 flex flex-col justify-center items-center">
-            <h2 className="text-white text-center text-3xl font-bold px-6">
-              Autoryzowane centrum blacharsko-lakiernicze
-            </h2>
-            <div className="w-2/4 bg-white h-1 mb-20" />
-            <h3 className="text-white text-center text-3xl font-bold">
-              Miałeś szkodę?
-            </h3>
-            <div className="border-2 border-orange-400 px-12 py-3">
-              <span className="text-orange-400 text-2xl font-bold">
-                Zadzwoń
-              </span>
+          <div className="min-h-screen-3/4 flex flex-col justify-around items-center py-16">
+            <div className="flex flex-col items-center">
+              <h1 className="text-white text-center font-bold px-6">
+                Autoryzowane centrum blacharsko-lakiernicze
+              </h1>
+              <div className="w-2/4 bg-white h-1 mb-20" />
+            </div>
+            <div className="flex flex-col items-center">
+              <h2 className="text-white text-center font-bold">
+                Miałeś szkodę?
+              </h2>
+              <div className="border-2 border-orange-400 px-12 py-3">
+                <span className="text-orange-400 text-2xl font-bold">
+                  Zadzwoń
+                </span>
+              </div>
             </div>
           </div>
         </div>
