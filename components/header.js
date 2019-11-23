07f42e29376e53ff6f20e3baedda8d69743a328d
Kamil Jakubus <kamiljkbz@protonmail.com>
2019-11-23 17:51:09 +0100

    Add sliding menu

--- components/header.js
+++ components/header.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef } from "react";
+import ReactDOM from "react-dom";
 
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaAngleDoubleRight } from "react-icons/fa";
 
 function Header({}) {
-  const showMenu = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = useCallback(() => setShowMenu(!showMenu), [showMenu]);
 
   return (
-    <div className="absolute top-0 w-full bg-transparent z-10">
-      <div className="max-w-screen-xl flex justify-between items-center bg-transparent z-10 h-16 mx-auto px-3">
+    <div className="absolute top-0 w-full bg-transparent z-20">
+      <div className="max-w-screen-xl flex justify-between items-center bg-transparent z-20 h-16 mx-auto px-3">
         <h1 className="font-bold text-gray-300">MKT SERWIS</h1>
-        <FaBars className="text-4xl text-gray-300 cursor-pointer" />
+        <div className="cursor-pointer" onClick={toggleMenu}>
+          {!showMenu ? (
+            <FaBars className="text-4xl text-gray-300" />
+          ) : (
+            <FaAngleDoubleRight className="text-4xl text-gray" />
+          )}
+        </div>
       </div>
+      <div
+        className="w-full bg-orange"
+        style={{
+          height: "calc(100vh - 4rem)",
+          transition: "transform 0.5s",
+          transform: showMenu ? "translateX(0)" : "translateX(100vw)"
+        }}
+      ></div>
     </div>
   );
 }
