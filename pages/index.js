a5452b7b8d33af11e43c8c817bc132fd46fd72a5
Mateusz Bernacik <mat.bernacik@gmail.com>
2019-11-24 17:23:12 +0100

    Add video

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,43 @@
-import React, {useRef} from 'react';
+import React, { useRef } from "react";
 
-import Layout from '../components/layout';
-import Header from '../components/header';
+import Layout from "../components/layout";
+import Header from "../components/header";
 
 function Home() {
   return (
     <Layout>
       <div className="h-screen">
-        <div
-          className="bg-cover"
-          style={{backgroundImage: 'url(/mobile1.jpeg)'}}>
-          <div className="min-h-screen-3/4 flex flex-col justify-around items-center py-16">
-            <div className="flex flex-col items-center">
-              <h1 className="text-white text-center font-bold px-3">
-                Autoryzowane centrum blacharsko-lakiernicze
-              </h1>
-              <div className="w-2/4 bg-white h-1 mb-20" />
-            </div>
-            <div className="flex flex-col items-center">
-              <h2 className="text-white text-center font-bold">
-                Miałeś szkodę?
-              </h2>
-              <div className="border-2 border-orange px-12 py-3">
-                <span className="text-orange text-2xl font-bold">Zadzwoń</span>
-              </div>
+        <video
+          autoplay="true"
+          loop="true"
+          playsinline="true"
+          className="absolute top-0 min-h-screen-3/4 object-left-top object-cover z-0"
+        >
+          <source src="/video1.mp4" type="video/mp4" />
+        </video>
+
+        <div className="min-h-screen-3/4 flex flex-col justify-around items-center py-16">
+          <div className="flex flex-col items-center z-10">
+            <h1 className="text-white text-center font-bold px-3">
+              Autoryzowane centrum blacharsko-lakiernicze
+            </h1>
+            <div className="w-2/4 bg-white h-1 mb-20" />
+          </div>
+          <div className="flex flex-col items-center">
+            <h2 className="text-white text-center font-bold z-10">
+              Miałeś szkodę?
+            </h2>
+            <div className="border-2 border-orange px-12 py-3 z-10">
+              <span className="text-orange text-2xl font-bold">Zadzwoń</span>
             </div>
           </div>
         </div>
+
         <img
           className="min-h-screen-1/4 object-contain px-8"
-          src={'/loga.png'}
+          src={"/loga.png"}
         />
+
         <Header />
       </div>
     </Layout>
