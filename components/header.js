342a440e5f349558d597a5a725e147d44545b34b
Kamil Jakubus <kamiljkbz@protonmail.com>
2019-11-24 15:16:17 +0100

    Header color fixes

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
-import React, { useState, useCallback, useRef } from "react";
-import ReactDOM from "react-dom";
+import React, {useState, useCallback, useRef} from 'react';
+import ReactDOM from 'react-dom';
 
-import { FaBars, FaAngleDoubleRight } from "react-icons/fa";
+import {FaBars, FaAngleDoubleRight} from 'react-icons/fa';
 
 function Header({}) {
   const [showMenu, setShowMenu] = useState(false);
@@ -11,23 +11,22 @@ function Header({}) {
   return (
     <div className="absolute top-0 w-full bg-transparent z-20">
       <div className="max-w-screen-xl flex justify-between items-center bg-transparent z-20 h-16 mx-auto px-3">
-        <h1 className="font-bold text-gray-300">MKT SERWIS</h1>
+        <h1 className="font-bold text-white">MKT SERWIS</h1>
         <div className="cursor-pointer" onClick={toggleMenu}>
           {!showMenu ? (
-            <FaBars className="text-4xl text-gray-300" />
+            <FaBars className="text-4xl text-white" />
           ) : (
-            <FaAngleDoubleRight className="text-4xl text-gray" />
+            <FaAngleDoubleRight className="text-4xl text-white" />
           )}
         </div>
       </div>
       <div
         className="w-full bg-orange"
         style={{
-          height: "calc(100vh - 4rem)",
-          transition: "transform 0.5s",
-          transform: showMenu ? "translateX(0)" : "translateX(100vw)"
-        }}
-      ></div>
+          height: 'calc(100vh - 4rem)',
+          transition: 'transform 0.5s',
+          transform: showMenu ? 'translateX(0)' : 'translateX(100vw)',
+        }}></div>
     </div>
   );
 }
