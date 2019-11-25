3783c04df2febf7ac272ed7cb878710aca7736fb
Mateusz Bernacik <mat.bernacik@gmail.com>
2019-11-25 22:16:08 +0100

    Add side menu content

--- components/header.js
+++ components/header.js
@@ -1,11 +1,11 @@
-import React, {useState, useCallback, useRef} from 'react';
+import React, { useState, useCallback, useRef } from 'react'
 
-import {FaBars, FaAngleDoubleRight} from 'react-icons/fa';
+import { FaBars, FaAngleDoubleRight } from 'react-icons/fa'
 
 function Header({}) {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false)
 
-  const toggleMenu = useCallback(() => setShowMenu(!showMenu), [showMenu]);
+  const toggleMenu = useCallback(() => setShowMenu(!showMenu), [showMenu])
 
   return (
     <div className="absolute top-0 w-full bg-transparent z-20">
@@ -20,14 +20,21 @@ function Header({}) {
         </div>
       </div>
       <div
-        className="w-full bg-orange"
+        className="w-full bg-orange p-6"
         style={{
           height: 'calc(100vh - 4rem)',
           transition: 'transform 0.5s',
           transform: showMenu ? 'translateX(0)' : 'translateX(100vw)',
-        }}></div>
+        }}
+      >
+        <div className="footer-item">START</div>
+        <div className="footer-item">KONTAKT</div>
+        <div className="footer-item ">USŁUGI</div>
+        <div className="footer-item">POLITYKA PRYWATNOŚCI</div>
+        <div className="footer-item">USŁUGI</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
